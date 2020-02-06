d2739b41410e778d2ed98727d8e59cc816478fd9
moumitabis <60727543+moumitabis@users.noreply.github.com>
2020-02-06 14:06:29 +0530

    Abstraction

---
+++ abstraction/CheckAbstraction.java
@@ -0,0 +1,12 @@
+package abstraction;
+
+public class CheckAbstraction extends AbstractDemo {
+    @Override
+    void showData() {
+
+    }
+    public static void main(String[] args) {
+        CheckAbstraction abstraction=new CheckAbstraction();
+        abstraction.showData();
+    }
+}
