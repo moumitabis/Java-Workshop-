8fb972eeeac300b097879e228f603593ee09c23a
moumitabis <60727543+moumitabis@users.noreply.github.com>
2020-02-06 12:02:22 +0530

    First Program

---
+++ Demo.java
@@ -0,0 +1,19 @@
+public class Demo {
+
+        void showData(){
+
+            System.out.printf("Hello");
+
+
+        }
+
+
+        public static void main(String[] args) {
+
+            Demo demo=new Demo();
+            demo.showData();
+        }
+
+    }
+
+
