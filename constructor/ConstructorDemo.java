b61f9a473fa14e3489c0a01d5f10fa50f3f523c9
moumitabis <60727543+moumitabis@users.noreply.github.com>
2020-02-06 11:37:34 +0530

    Keywords

---
+++ constructor/ConstructorDemo.java
@@ -0,0 +1,13 @@
+package constructor;
+
+public class ConstructorDemo {
+	ConstructorDemo() {
+		System.out.println("default Constuctor");
+	}
+	ConstructorDemo(int a) {
+		
+	}
+	public static void main(String[] args) {
+		ConstructorDemo constructorDemo=new ConstructorDemo();
+	}
+}
