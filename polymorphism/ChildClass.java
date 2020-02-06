82948bda7d99460372f04d7b42081f05c549064f
moumitabis <60727543+moumitabis@users.noreply.github.com>
2020-02-06 11:22:55 +0530

    Poymorphism $ Recursion

---
+++ polymorphism/ChildClass.java
@@ -0,0 +1,11 @@
+package polymorphism;
+
+public class ChildClass extends ParentClass {
+    ChildClass() {
+        System.out.println("Hello!");
+    }
+    public static void main(String[] args) {
+        ChildClass child=new ChildClass();
+
+    }
+}
