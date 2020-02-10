e3e30abb5cb1ed9712064b3c7255c03ba6eaa441
moumitabis <60727543+moumitabis@users.noreply.github.com>
2020-02-10 11:54:05 +0530

    Inner class

---
+++ innerclass/MethodInnerClass.java
@@ -0,0 +1,19 @@
+package innerclass;
+
+public class MethodInnerClass {
+    private int password=12345;
+    void displayPrivateData() {
+        class CheckMethodInnerClass {
+            void showPassword() {
+                System.out.println(password);
+            }
+
+        }
+        CheckMethodInnerClass checkMethodInnerClass= new CheckMethodInnerClass();
+        checkMethodInnerClass.showPassword();
+    }
+    public static void main(String[] args) {
+        MethodInnerClass methodInnerClass= new MethodInnerClass();
+        methodInnerClass.displayPrivateData();
+    }
+}
